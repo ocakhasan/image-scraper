e3f66db18bb8dc61498759131affef39c0329b97
Hasan Ocak <ocakhasan99@gmail.com>
2021-08-14 20:45:10 +0300

    change utils to scraper

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/ocakhasan/image-scraper/utils"
+
+	"github.com/ocakhasan/image-scraper/scraper"
 )
 
 var folderFlag = flag.String("f", "imageFolder", "folder for the images")
 var websiteFlag = flag.String("w", "https://google.com", "website to crawl images")
 
-func main(){
+func main() {
 	flag.Parse()
-	err := utils.GetImages(*websiteFlag, *folderFlag)
+	err := scraper.GetImages(*websiteFlag, *folderFlag)
 	if err != nil {
 		fmt.Printf("err: %v", err)
 	}
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,4 +1,4 @@
-package utils
+package scraper
 
 import (
 	"fmt"
